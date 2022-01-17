ecb43f5e192d9eff3c5f48a3398ed0db26e34b42
Anton <tinetoon@mail.ru>
2022-01-17 17:10:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androinforjava/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.androinforjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
